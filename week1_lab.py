5e2555aa7c508b95db6c42e00a4588da357010ed
Rayyan <Rayyan_007@outlook.com>
2025-10-24 16:19:04 +0100

    Feat: Complete Week 1 lab exercises

---
+++ week1_lab.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+
+# string
+student_name = "Alex"
+
+# integer
+student_age = 20
+
+# print variables
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+
+# A simple calculation
+num1 = 15
+num2 = 10
+total = num1 + num2
+print("The sum is:", total)
