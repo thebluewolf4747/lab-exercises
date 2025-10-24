a3e5546b39f9e5c977268fb09756021139fcadbb
Rayyan <Rayyan_007@outlook.com>
2025-10-24 18:29:03 +0100

    Feat: Generate square and triangle patterns with nested loops (and asterisks)

--- week3_lab.py
+++ week3_lab.py
@@ -14,3 +14,16 @@ for cost in costs:
     total_cost += cost
 
 print(f"{total_cost:.2f}")
+
+square_size = 5     # for a square, width = height, so they are stored in the same variable (size)
+
+for row in range(square_size):
+    for col in range(square_size):
+        print("*",end=" ")
+    print()
+
+triangle_height = 5
+for row in range(triangle_height):
+    for col in range(row + 1):          # this will stop at the fourth iteration if the range was just row.
+        print("*",end=" ")
+    print()
