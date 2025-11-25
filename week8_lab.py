e8e522b7fb3c9ea649efa380eef60e7f3dbc8f44
Rayyan <Rayyan_007@outlook.com>
2025-11-25 10:54:20 +0000

    Completed Exercise 5

--- week8_lab.py
+++ week8_lab.py
@@ -62,12 +62,25 @@ fake_api_data = {
 url = "https://official-joke-api.appspot.com/random_joke"
 
 # 1. Send the request
-print("Calling the server...")
+# print("Calling the server...")
 response = requests.get(url)
 
 # 2. Check the status code (200 = Success, 404 = Not Found)
-print(f"Status Code: {response.status_code}")
+if response.status_code == 200:
+    # Convert raw text to a Python dictionary
+    joke_data = response.json()
 
-# 3. See the raw test data
-print("Raw data")
-print(response.text)
+    # Now extract the specific parts (Keys: 'setup' and 'punchline')
+    setup = joke_data["setup"]
+    punchline = joke_data["punchline"]
+
+    print("----------------")
+    print(f"Joke: {setup}")
+    print(f"Answer: {punchline}")
+    print("----------------")
+else:
+    print("Error: Could not fetch joke.")
+
+
+""" Exercise 5 """
+# See modified code from exercise 4
